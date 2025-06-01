fbdd699e62aaa4f5710dd6902ecbe32284a1ba10
SasukeGh <162058638+SasukeGh@users.noreply.github.com>
2025-06-01 10:01:56 +0000

    Update Footer.jsx

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -9,19 +9,19 @@ const Footer = () => {
     <footer className="footer">
       <div className="footer__logo">
         <img src={logo} alt="logo" />
-        <h5>MyPizza</h5>
-        <p>Best Pizzas in town, try it out!</p>
+        <h5>Conectar Restaurant</h5>
+        <p>Best Food in town, try it out!</p>
       </div>
       <div>
         <h5 className="footer__title mb-3">Delivery Time</h5>
         <ListGroup>
           <div className="delivery__time-item border-0 ps-0">
-            <span>Friday - Tuesday</span>
-            <p>10:00am - 11:00pm</p>
+            <span>Monday - Saturday</span>
+            <p>9:00am - 8:00pm</p>
           </div>
           <div className="delivery__time-item border-0 ps-0">
-            <span>Wednesday - Thursday</span>
-            <p>Off day</p>
+            <span>Sunday</span>
+            <p> This time differs due to church.</p>
           </div>
         </ListGroup>
       </div>
