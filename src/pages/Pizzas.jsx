475e7f815871a98d3550d00a2f0ffec48c6f144c
SasukeGh <162058638+SasukeGh@users.noreply.github.com>
2025-06-01 10:09:54 +0000

    Update Pizzas.jsx
    redir test2

--- src/pages/Pizzas.jsx
+++ src/pages/Pizzas.jsx
@@ -1,3 +1,12 @@
-useEffect(() => {
-  navigate('/menu');
-}, []);
+import { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+
+export default function Pizzas() {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    navigate('/menu', { replace: true }); // replace:true prevents back button returning to /pizzas
+  }, [navigate]);
+
+  return null; // nothing to render
+}
