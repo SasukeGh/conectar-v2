38b93dd02cc2bf3b7e429881d2f2a5c82a1626fb
SasukeGh <162058638+SasukeGh@users.noreply.github.com>
2025-06-04 18:29:25 +0000

    Update Routers.js

--- src/routes/Routers.js
+++ src/routes/Routers.js
@@ -6,8 +6,8 @@ import Pizzas from "../pages/Pizzas";
 import PizzaDetails from "../pages/PizzaDetails";
 import Cart from "../pages/Cart";
 import Checkout from "../pages/Checkout";
-import Menu from "../pages/menu"; // Make sure this import uses consistent casing
-import Dev from "../pages/dev";
+import Menu from "../pages/menu";
+import Dev from "../pages/dev"; // this is your redirect component
 
 const Routers = () => {
   return (
@@ -15,10 +15,11 @@ const Routers = () => {
       <Route path="/" element={<Navigate to="/home" />} />
       <Route path="/home" element={<Home />} />
       <Route path="/pizzas" element={<Pizzas />} />
-      <Route path="/menu" element={<Menu />} /> {/* Add this missing route */}
+      <Route path="/menu" element={<Menu />} />
       <Route path="/cart" element={<Cart />} />
       <Route path="/checkout" element={<Checkout />} />
       <Route path="/pizzas/:id" element={<PizzaDetails />} />
+      <Route path="/dev" element={<Dev />} /> {/* ✅ Add this line */}
     </Routes>
   );
 };
